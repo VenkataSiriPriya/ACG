1c11ead7eb1890dac9f3036ffcbd5a2951c9fb18
SiriPriya <2300030401@kluniversity.in>
2025-07-20 18:38:23 +0530

    Updated the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-// File: src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Home from './pages/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
@@ -19,6 +17,7 @@ import ManageUsers from './dashboards/ManageUsers';
 import NotFound from './Error/NotFound';
 import ProtectedRoute from './Error/ProtectedRoute';
 
+
 export default function App() {
   return (
     <Router>
@@ -28,20 +27,17 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          
-          {/* Protected routes */}
-          <Route path="/places" element={<ProtectedRoute><Places /></ProtectedRoute>} />
-          <Route path="/places/:id" element={<ProtectedRoute><PlaceDetails /></ProtectedRoute>} />
-          <Route path="/submit" element={<ProtectedRoute><SubmitPlace /></ProtectedRoute>} />
-          <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
-          <Route path="/contact" element={<ProtectedRoute><Contact /></ProtectedRoute>} />
-          <Route path="/tips" element={<ProtectedRoute><Tips /></ProtectedRoute>} />
-          <Route path="/forgot-password" element={<ProtectedRoute><ForgotPassword /></ProtectedRoute>} />
-          <Route path="/admin-dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-          <Route path="/manage-users" element={<ProtectedRoute><ManageUsers /></ProtectedRoute>} />
-          
-          {/* Catch-all route */}
+          <Route path="/places" element={<Places />} />
+          <Route path="/places/:id" element={<PlaceDetails />} />
+          <Route path="/submit" element={<SubmitPlace />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/tips" element={<Tips />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/manage-users" element={<ManageUsers />} />
           <Route path="*" element={<NotFound />} />
+
         </Routes>
       </main>
       <Footer />
